ae69ca2f662d71fcc87e4b275267e3365e4edef2
Alex <alex_ngo@live.com>
2015-12-04 19:45:11 -0800

    added tips, writes to json file, and cleaned up code style

--- MarkovChain.py
+++ MarkovChain.py
@@ -1,51 +1,80 @@
 import random
 import rethinkdb as r
+import json
 
-def getCorpus():
+def get_corpus(table_name):
     corpus = ''
     r.connect(host="localhost", db="yelp", port=28015).repl()
 
-    reviewCursor = r.table("reviews").run()
+    review_cursor = r.table(table_name).run()
 
     y = 0
-
-    for item in reviewCursor:
-        if y > 2000:
+    for item in review_cursor:
+        if y > 1000:
             break
         y += 1
         corpus = corpus + ' ' + item['text']
     return corpus
 
 
-def makeFreqDict(corpus):
-    freqDict = dict()
+def make_freq_dict(corpus):
+    freq_dict = dict()
     prev = ''
     cur = ''
     for word in corpus.split(' '):
         key = (prev, cur)
-        if key not in freqDict:
-            freqDict[key] = {word: 1}
+        if key not in freq_dict:
+            freq_dict[key] = {word: 1}
         else:
-            if word in freqDict[key]:
-                freqDict[key][word] += 1
+            if word in freq_dict[key]:
+                freq_dict[key][word] += 1
             else:
-                freqDict[key][word] = 1
+                freq_dict[key][word] = 1
 
         prev = cur
         cur = word
-    return freqDict
+    return freq_dict
 
-def getReview(freqDict) :
-    wordArray = []
+
+def get_text(freq_dict):
+    word_array = []
     prev = ''
     cur = ''
 
-    for i in range(50):
-        word = random.choice([x for x in freqDict[(prev, cur)] for y in range(freqDict[(prev, cur)][x])])
-        wordArray.append(word)
+    for _ in range(50):
+        try:
+            word = random.choice([x for x in freq_dict[(prev, cur)] for y in range(freq_dict[(prev, cur)][x])])
+            word_array.append(word)
+            prev = cur
+            cur = word
+        except KeyError:
+            word_array.append('.')
+            prev = ''
+            cur = ''
+    while not cur.endswith('.'):
+        word = random.choice([x for x in freq_dict[(prev, cur)] for y in range(freq_dict[(prev, cur)][x])])
+        word_array.append(word)
         prev = cur
         cur = word
-    print(" ".join(wordArray))
+    return " ".join(word_array)
 
-for i in range(5):
-    getReview(makeFreqDict(getCorpus()))
+
+def main():
+    review_freq_dict = make_freq_dict(get_corpus("reviews"))
+    tip_freq_dict = make_freq_dict(get_corpus("tips"))
+
+    reviews = []
+    tips = []
+    for _ in range(100):
+        reviews.append(get_text(review_freq_dict))
+        tips.append(get_text(tip_freq_dict))
+
+    reviews = {'reviews': reviews}
+    tips = {'tips': tips}
+    with open('reviews.json', 'w') as outfile:
+        json.dump(reviews, outfile)
+    with open('tips.json', 'w') as outfile:
+        json.dump(reviews, outfile)
+
+if __name__ == "__main__":
+    main()
